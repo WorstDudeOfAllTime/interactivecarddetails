de6460e53fc12217cf7bb86513cb6e978f76225d
WorstDudeOfAllTime <Krisbertolino@gmail.com>
2022-08-13 17:46:36 -0400

    FEM Interactive Card

---
+++ form.js
@@ -0,0 +1,38 @@
+const submitBtn = document.getElementById("submit-btn");
+const cardNum = document.getElementById("card-number");
+const getHolder = document.getElementById("get-holder");
+const getNum = document.getElementById("get-num");
+const cardHold = document.getElementById("card-holder");
+const getMonth = document.getElementById("get-MM");
+const getYear = document.getElementById("get-YY");
+const expiry = document.getElementById("exp-holder");
+const cardForm = document.getElementById("card-form");
+
+const validator = (field, length = 1) => {
+  if (!field || field.value.length < length) {
+    alert(`${field.id} needs more girth`);
+    return false;
+  }
+  return true;
+};
+getNum.addEventListener("keydown", () => {
+  if (
+    getNum.value.length === 4 ||
+    getNum.value.length === 9 ||
+    getNum.value.length === 14
+  ) {
+    getNum.value = getNum.value + " ";
+  }
+});
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!validator(getHolder)) return false;
+  if (!validator(getNum, 19)) return false;
+  if (!validator(getMonth, 2)) return false;
+  if (!validator(getYear, 2)) return false;
+  cardHold.innerHTML = getHolder.value;
+  cardNum.innerHTML = getNum.value;
+  expiry.innerHTML = `${getMonth.value}/${getYear.value}`;
+  cardForm.reset();
+});
